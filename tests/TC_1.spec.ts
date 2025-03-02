f6fbff97dbec9f98c9a1cbcedb1c76959bd1c020
manuo <ojhapranav1999@gmail.com>
2025-03-02 23:54:02 +0530

    updating tests

--- tests/TC_1.spec.ts
+++ tests/TC_1.spec.ts
@@ -1,19 +1,28 @@
 import { test, expect } from '@playwright/test';
 
-import { environment_variables } from '../environment/environment';
+import { environment_variables, songs_array, songs_details } from '../environment/environment';
+import 'dotenv/config';
 
 
 const obj = new environment_variables();
 var playlist_name = 'Coding Music'
 
+var external_songs_array = new songs_details();
+
+
 
 test('Login process',async({page})=>{
+
+    console.log('-----NAVIGATING TO SPOTIFY HOMEPAGE ------')
+
     await page.goto('https://open.spotify.com/');
     await page.getByTestId('login-button').click();
+
+    console.log('------LOGIN PROCESS STARTING-----')
     await page.locator('#login-username').fill(obj.getusername());
     await page.locator('#login-password').fill(obj.getpassword());
     await page.locator('#login-button').click();
-    console.log('Login process completed');
+    console.log('------LOGIN PROCESS COMPLETE------');
 
     await page.getByRole('button', { name: 'Coding Music Playlist' }).click();
 
@@ -25,20 +34,23 @@ test('Login process',async({page})=>{
 
     let songs_list_name:{id:number,song_name:string} []= []
     let i = 0 ;
+
+    //Iterating through the songs grid to obtain the songs_array .  
     songs_list.forEach(async Element=>{
-        // console.log(await Element.locator('div').nth(1).locator('button').getAttribute('aria-label'));
+        
         let temp = await Element.locator('div').nth(1).locator('button').getAttribute('aria-label');
-        // console.log(temp)
+        external_songs_array.setSongName(temp);
         if(temp!==undefined){
             songs_list_name[i] ={
                 id:i,
                 song_name:temp||''
             }
         }
-        i++ ;
-        // console.log(songs_list_name);
+        i++ ; 
     })
-    
-    page.pause()
 
 })
+
+
+
+
--- tests/TC_2.spec.ts
+++ tests/TC_2.spec.ts
@@ -1,22 +1,39 @@
-import {test} from '@playwright/test'
+import {expect, test} from '@playwright/test'
+import { songs_array, songs_links_array } from '../environment/environment';
+import { songs_links } from '../environment/environment';
 
-test.skip('Scraping spotify for downloads', async({page})=>{
-    console.log('LETS BEGIN MUSIC SCRAPING');
 
-    await page.goto('https://open.spotify.com/');
-    await page.getByTestId('component-shelf').nth(3).getByTestId('grid-container').locator('div').first().click();
+var links_obj = new songs_links();
 
-    // const printer = await page.getByTestId('tracklist-row').all();
-    // console.log('-----------------',printer);
-    const song_array = await page.getByTestId('tracklist-row').first().getByRole('gridcell').nth(1).locator('div').first().innerText();
-    // console.log(song_array)
-    // await page.waitForLoadState();
+test('Scraping spotify for downloads', async({page})=>{
+    console.log(`Verifying if we have the songs array`);
+    expect(songs_array).toBeTruthy();
+    console.log(songs_array)
+})
 
-    // console.log(await page.getByTestId('tracklist-row').all());
-    let try1 = await page.getByTestId('tracklist-row').all();
-    // console.log(try1);
-    try1.forEach(async element => {
-        console.log(await element.locator('div').nth(1).locator('button').getAttribute('aria-label')); 
-    });
+test.only('Downloading the songs one by one',async({page})=>{
+    test.setTimeout(20000);
+    await page.goto('https://www.youtube.com/');
+    const tempsongs_array = ['Flight by Tristam, Braken',
+        'Forbidden Voices by Martin Garrix',
+        'The Only Way Is Up by Martin Garrix, Tiësto',
+        'Take My Breath Away by Alesso']
+    // await page.locator('.ytSearchboxComponentInput').fill('fsdafs');
+        tempsongs_array.forEach( async Element=>{
+            console.log(await page.locator('.ytSearchboxComponentInput'));
+            console.log(Element);
+            await page.locator(".ytSearchboxComponentInput").fill(Element);
+            page.keyboard.press('return')
+            await page.locator('#contents').locator('ytd-video-renderer').first().click();
+            let temp = await page.url();
+            links_obj.setsongs_links(temp);
+    })
+    
+    await page.pause();
+    
+})
 
+test.only('verifying links array',({page})=>{
+    console.log(songs_links_array);
+    expect(songs_links_array).toBeTruthy();
 })
